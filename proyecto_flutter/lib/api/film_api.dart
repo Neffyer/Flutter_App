705dbc877fca7578ea6ff11293e2648d4020737a
Joel Maldonado Salvador <maldonadosalvadorjoel@gmail.com>
2023-12-02 18:52:50 +0100

    api update (link added)

--- proyecto_flutter/lib/api/film_api.dart
+++ proyecto_flutter/lib/api/film_api.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:proyecto_flutter/models/film.dart';
 
 Future<List<Film>> apiLoadUsers() async {
-  final uri = Uri.parse("https://randomuser.me/api/");
+  final uri = Uri.parse("https://api.themoviedb.org/3/movie/11?api_key=ce3dded01779db04a7820eb05d6b3095");
   final response = await http.get(uri);
   final json = jsonDecode(response.body);
   final jsonUserList = json["results"];
