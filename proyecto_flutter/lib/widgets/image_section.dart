0e96ab98d1105e7840a14e85d8a10c4dbd94ee0d
DaniMariages <99950347+DaniMariages@users.noreply.github.com>
2023-12-12 11:30:24 +0100

    Bar and button fix

--- proyecto_flutter/lib/widgets/image_section.dart
+++ proyecto_flutter/lib/widgets/image_section.dart
@@ -10,50 +10,48 @@ class Image_Section extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
-    return Scaffold(
-      body: SizedBox(
-        width: screenSize.width,
-        height: 400,
-        child: Stack(
-          children: [
-            SizedBox(
-              width: screenSize.width,
-              height: 400,
-              child: Image.network(
-                'https://i.blogs.es/3e839c/star-wars/1366_2000.jpg',
-                fit: BoxFit.cover,
-              ),
+    return SizedBox(
+      width: screenSize.width,
+      height: 400,
+      child: Stack(
+        children: [
+          SizedBox(
+            width: screenSize.width,
+            height: 400,
+            child: Image.network(
+              'https://i.blogs.es/3e839c/star-wars/1366_2000.jpg',
+              fit: BoxFit.cover,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Back_Button(),
-                Share_Button(),
-              ],
-            ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20, bottom: 10),
-                child: Text(
-                  "Star Wars",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(1.0, 1.0),
-                        blurRadius: 1.0,
-                        color: Colors.black,
-                      ),
-                    ],
-                  ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Back_Button(),
+              Share_Button(),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, bottom: 10),
+              child: Text(
+                "Star Wars",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      offset: Offset(1.0, 1.0),
+                      blurRadius: 1.0,
+                      color: Colors.black,
+                    ),
+                  ],
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
