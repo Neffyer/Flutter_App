b01dc6d9cae4b781a25e28fcdb4fb26595816b3c
Joel Maldonado Salvador <maldonadosalvadorjoel@gmail.com>
2023-12-28 00:23:37 +0100

    horizontal scroll view added to some zones

--- proyecto_flutter/lib/widgets/mas_cosas.dart
+++ proyecto_flutter/lib/widgets/mas_cosas.dart
@@ -5,14 +5,17 @@ class MasCosas extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-      child: Container(
-        height: 100,
-        width: 100,
-        decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.circular(20),
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+        child: Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(20),
+          ),
         ),
       ),
     );
