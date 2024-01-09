6d2a15d67c4a427f9c534d8e789febc8f5a9acd3
DaniMariagesUPC <99950347+DaniMariages@users.noreply.github.com>
2024-01-09 18:23:52 +0100

    Horizontal scroll en proceso

--- proyecto_flutter/lib/widgets/cositas.dart
+++ proyecto_flutter/lib/widgets/cositas.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_flutter/api/film_api.dart';
+import 'package:proyecto_flutter/models/film_class.dart';
+import 'package:proyecto_flutter/widgets/film_class_item.dart';
 import 'package:proyecto_flutter/widgets/mas_cosas.dart';
 
 class Cositas extends StatelessWidget {
@@ -6,15 +9,42 @@ class Cositas extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        MasCosas(),
-        MasCosas(),
-        MasCosas(),
-        MasCosas(),
-        MasCosas(),
-        MasCosas(),
-      ],
+    return  Row(
+      //SE PETA AL CARGAR LA API DOS VECES      
+      // children: [
+      //   const SizedBox(height: 20),
+      //    Padding(
+      //         padding: const EdgeInsets.only(top: 290.0),
+      //         child: FutureBuilder(
+      //           future: apiLoadPhoto(),
+      //           builder: (
+      //             BuildContext context,
+      //             AsyncSnapshot<List<Film>> snapshot,
+      //           ) {
+      //             if (!snapshot.hasData) {
+      //               return const Center(
+      //                 child: CircularProgressIndicator(),
+      //               );
+      //             }
+      //             final userList = snapshot.data!;
+      //             return ListView.builder(
+      //               itemCount: userList.length,
+      //               itemBuilder: (BuildContext context, int index) {
+      //                 return FilmListItem(film: userList[index]);
+      //               },
+      //             );
+      //           },
+      //         ),
+      //       ),
+      // ],
+       children: [
+         MasCosas(),
+         MasCosas(),
+         MasCosas(),
+         MasCosas(),
+         MasCosas(),
+         MasCosas(),
+       ],
     );
   }
 }
